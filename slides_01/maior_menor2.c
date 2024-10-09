650dac3be21d8acac77282666380a0f3eb4564a0
Henryque02 <henryque.affiune10@gmail.com>
2024-10-09 15:12:53 -0300

    att

--- slides_01/maior_menor2.c
+++ slides_01/maior_menor2.c
@@ -1,25 +1,29 @@
 /*Elabore um programa que faça leitura de vários números inteiros, até que se digite um número negativo. 
-O programa deve mostrar todos os números digitados (sem o negativo)*/
+O programa tem que retornar o maior e o menor número lido*/
 
 #include<stdio.h>
 
 int main(){
     
-    int vetor[20], n, i = 0, j;
+    int vetor[20], n, i = 0, j, maior, menor;
     while (n >= 0)
     {
         printf("Digite um numero positivo para continuar e um negativo para parar: ");
         scanf("%d", &n);
         vetor[i] = n;
+        if (n > maior)
+        {
+            maior = n;
+        }
+        if (n < menor && 0 <= n)
+        {
+            menor = n;
+        }
         i++;
 
     }
 
-    printf("Numeros digitados:\n");
-    for ( j = 0; j < i-1 ; j++)
-    {        
-        printf("\n%d", vetor[j]);
-    }
+    printf("\nO maior numero digitado foi %d e o menor foi %d:\n", maior, menor);
     
     return 0;
 }
