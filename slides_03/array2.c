eaf23c7665a994ce3455961c9cffdf971d2a5965
Henryque02 <henryque.affiune10@gmail.com>
2024-10-17 16:53:28 -0300

    Update array2.c

--- slides_03/array2.c
+++ slides_03/array2.c
@@ -32,3 +32,44 @@ int main(){
 
     return 0;
 }
+
+/*
+#include <stdio.h>
+#define limit 10
+
+int main(){
+    int numeros[limit], aux; 
+    int maior = 0, menor = 0;
+
+    printf("Insira um numero: ");
+    for(int i=0; i<limit; i++){
+        scanf("%d", &numeros[i]);   
+    }
+
+    // Ordenar para saber qual sera o maior
+    for(int i=0; i<limit; i++){
+         for (int j=0; j<limit-i-1; j++){
+            if(numeros[j] > numeros[j+1]){
+            aux = numeros[j];
+            numeros[j] = numeros[j+1];
+            numeros[j+1] = aux;
+            }
+        }
+    }
+    maior = numeros[9];
+    menor = numeros[0];
+    printf("\n");
+    
+    // Printing 
+    
+    printf("Elementos do vetor: ");
+    for(int i=0; i<limit; i++){
+        printf("[%d] ", numeros[i]);
+    }
+    printf("\n");
+    printf("Maior = %d\n", maior);
+    printf("Menor = %d\n", menor);
+
+
+    return 0;
+} */
