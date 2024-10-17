52e72f294ef2f260344f4050f3895351cfcfea0a
Henryque02 <henryque.affiune10@gmail.com>
2024-10-17 08:49:14 -0300

    att

--- slides_01/ascii.c
+++ slides_01/ascii.c
@@ -1,1 +1,10 @@
 /*Fazer um programa que mostre o código ASCII correspondente aos caracteres gamer WASD*/
+
+#include<stdio.h>
+
+int main(){
+
+    printf("A seguir o codigo ASCII correspondente aos caracteres:\nW : %d\nA : %d\nS : %d\nD : %d", 'W', 'A', 'S', 'D');
+
+    return 0;
+}
