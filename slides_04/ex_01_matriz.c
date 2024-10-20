c95b665dbd9f5e46fb4f66635cc5bafc9f059bcd
Henryque02 <henryque.affiune10@gmail.com>
2024-10-20 11:34:59 -0300

    Update ex_01_matriz.c

--- slides_04/ex_01_matriz.c
+++ slides_04/ex_01_matriz.c
@@ -4,16 +4,3 @@ Verifique qual desses números é maior
 Imprima os elementos da matriz e, em seguida, o seu maior elemento
 (*) BONUS: Ingresse a ordem da matriz como parâmetro do programa*/
 
-#include <stdio.h>
-
-int M,N;
-
-int main(int argc, char *argv[]) {
-    printf("Número de argumentos: %d\n", argc);
-
-    for (int i = 0; i < argc; i++) {
-        printf("Argumento %d: %s\n", i, argv[i]);
-    }
-
-    return 0;
-}
