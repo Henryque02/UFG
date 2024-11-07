b3df8dc709a6433824d9fded9f89a54185e54580
Henryque02 <henryque.affiune10@gmail.com>
2024-11-07 10:38:31 -0300

    Update malloc.c

---
+++ slides_06/malloc.c
@@ -0,0 +1,25 @@
+/*Escreva um programa para armazenar um nome de até N bytes digitados pelo usuário.
+Requisitos:
+Verifique que a memória seja alojada corretamente
+Caso não seja informe ao usuário e termine o programa
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int n;
+
+    printf("Digite o numero de bytes que seu nome pode ter: ");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+    if (arr == NULL)
+    {
+        printf("Erro ao alocar memoria!\n");
+        return 1;
+    }
+    
+    return 0;
+}
