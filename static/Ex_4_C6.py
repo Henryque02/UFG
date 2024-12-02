983abd9e1673f74461a613ef28eb8dab9ef8e242
Henryque02 <henryque.affiune10@gmail.com>
2024-12-02 18:33:04 -0300

    att

---
+++ static/Ex_4_C6.py
@@ -0,0 +1,16 @@
+"""
+Para a avaliação da solução computacional apresentada, será analisada:  
+1.  A organização da entrada de dados e a organização da saída de dados,  
+2.  A organização do código-fonte, que precisa ser legível e identado,  
+3.  A capacidade do programa de realizar dos cálculos com a precisão da máquina e sem 
+erros algébricos.
+4.  A inexistência de erros conceituais nos conceitos físicos e matemáticos.
+5.  A capacidade do programa de ser adaptável para resolver um problema similar.
+6.  As questões devem ser resolvidas integralmente, de acordo com essas instruções e os 
+respectivos enunciados.  
+
+Os nomes dos participantes do grupo devem estar no cabeçalho, como foi apresentado no 
+exemplo, na forma de interface com o usuário, que significa imprimir na tela as informações ao 
+leitor.  
+O uso de instruções básicas da linguagem é recomendado. 
+"""
